45e77607e29d55954ed2d616c608d6de74270d92
george goglodze <c-george.goglodze@basecapanalytics.com>
2022-07-03 19:31:17 -0400

    added stock db and modified file

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from datetime import datetime
 from multiprocessing import connection
 import sqlite3
 
-connection = sqlite3.connect("stock.db")
-
 # Function to insert records in sql table
 def insert_data(stock_name, trade_type, premium_amount, trade_date):
     connection = sqlite3.connect("stock.db")
     cursor = connection.cursor()
     #cursor.execute('''INSERT INTO stock (stock_name, trade_type, premium_amount, trade_date) VALUES ( 'TSLA', 'CALL',1000,'2022-06-02 10:00:00')''')   
-    insert = f'''INSERT INTO stock (stock_name, trade_type, premium_amount, trade_date) VALUES ('{stock_name}', '{trade_type}', '{premium_amount}', '{trade_date}')'''
+    insert = f'''INSERT INTO stocks (stock_name, trade_type, premium_amount, trade_date) VALUES ('{stock_name}', '{trade_type}', '{premium_amount}', '{trade_date}')'''
     cursor.execute(insert)   
     connection.commit()
     print("Records have been saved")
@@ -23,18 +21,18 @@ def get_data(month):
     cursor = connection.cursor()
     #cursor.execute("SELECT * FROM stock")
     #cursor.execute("select * from stock where trade_date < date('now','-0 days')")
-    cursor.execute(f"SELECT * FROM stock WHERE strftime('%m', trade_date) = '{month}'")
+    cursor.execute(f"SELECT * FROM stocks WHERE strftime('%m', trade_date) = '{month}'")
     return cursor.fetchall()
 
 
-
-data = get_data('05')
-print(len(data))
-print(data)
+# Choose month to see records
+data = get_data('07')
+print(data, len(data))
+total = 0
 for i in data:
     print(i[2])
+    total += i[2]
+print("Total:", total)
 
-
-
-
-#insert_data('TSLA','CALL',1500,'2022-05-04 11:00:00')
+# Uncomment this line if u like to insert data
+#insert_data('TSLA','CALL',1055,'2022-07-01 11:00:00')
