692e1daccbc941748a572bc3575dc34dd820a039
george goglodze <c-george.goglodze@basecapanalytics.com>
2022-07-03 19:17:46 -0400

    added new files

--- main.py
+++ main.py
@@ -2,33 +2,35 @@ from datetime import datetime
 from multiprocessing import connection
 import sqlite3
 
+connection = sqlite3.connect("stock.db")
+
+# Function to insert records in sql table
 def insert_data(stock_name, trade_type, premium_amount, trade_date):
-    
     connection = sqlite3.connect("stock.db")
     cursor = connection.cursor()
-    # the line below is a good working cursor insert example
     #cursor.execute('''INSERT INTO stock (stock_name, trade_type, premium_amount, trade_date) VALUES ( 'TSLA', 'CALL',1000,'2022-06-02 10:00:00')''')   
     insert = f'''INSERT INTO stock (stock_name, trade_type, premium_amount, trade_date) VALUES ('{stock_name}', '{trade_type}', '{premium_amount}', '{trade_date}')'''
     cursor.execute(insert)   
     connection.commit()
-    cursor.execute("SELECT * FROM stock")
-    return cursor.fetchall()
+    print("Records have been saved")
+    #cursor.execute("SELECT * FROM stock")
+    #return cursor.fetchall()
 
 
+# Function to get stock trading data
 def get_data(month):
     connection = sqlite3.connect("stock.db")
     cursor = connection.cursor()
     #cursor.execute("SELECT * FROM stock")
     #cursor.execute("select * from stock where trade_date < date('now','-0 days')")
     cursor.execute(f"SELECT * FROM stock WHERE strftime('%m', trade_date) = '{month}'")
-    
     return cursor.fetchall()
+
 
 
 data = get_data('05')
 print(len(data))
 print(data)
-
 for i in data:
     print(i[2])
 
