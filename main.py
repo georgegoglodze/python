0b179cb6574a9fee36d4a19f2219a2a337af7ff2
george goglodze <c-george.goglodze@basecapanalytics.com>
2022-07-03 11:44:20 -0400

    added main

---
+++ main.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from multiprocessing import connection
+import sqlite3
+
+def insert_data(stock_name, trade_type, premium_amount, trade_date):
+    
+    connection = sqlite3.connect("stock.db")
+    cursor = connection.cursor()
+    # the line below is a good working cursor insert example
+    #cursor.execute('''INSERT INTO stock (stock_name, trade_type, premium_amount, trade_date) VALUES ( 'TSLA', 'CALL',1000,'2022-06-02 10:00:00')''')   
+    insert = f'''INSERT INTO stock (stock_name, trade_type, premium_amount, trade_date) VALUES ('{stock_name}', '{trade_type}', '{premium_amount}', '{trade_date}')'''
+    cursor.execute(insert)   
+    connection.commit()
+    cursor.execute("SELECT * FROM stock")
+    return cursor.fetchall()
+
+
+def get_data(month):
+    connection = sqlite3.connect("stock.db")
+    cursor = connection.cursor()
+    #cursor.execute("SELECT * FROM stock")
+    #cursor.execute("select * from stock where trade_date < date('now','-0 days')")
+    cursor.execute(f"SELECT * FROM stock WHERE strftime('%m', trade_date) = '{month}'")
+    
+    return cursor.fetchall()
+
+
+data = get_data('05')
+print(len(data))
+print(data)
+
+for i in data:
+    print(i[2])
+
+
+
+
+#insert_data('TSLA','CALL',1500,'2022-05-04 11:00:00')
